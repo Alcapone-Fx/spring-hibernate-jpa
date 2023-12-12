e2d06c989e8b7733dbf6324b97cc1628152900c8
Felix Navas <navasfx19@gmail.com>
2023-12-12 09:02:28 -0600

    :sparkles: Use create and get student

--- src/main/java/com/spring/hibernatejpa/HibernatejpaApplication.java
+++ src/main/java/com/spring/hibernatejpa/HibernatejpaApplication.java
@@ -1,5 +1,7 @@
 package com.spring.hibernatejpa;
 
+import com.spring.hibernatejpa.dao.StudentDAO;
+import com.spring.hibernatejpa.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,30 @@ public class HibernatejpaApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            System.out.println("Hello world!");
+            //this.createStudent(studentDAO);
+            this.getStudentById(studentDAO, 1);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        System.out.println("Creating student object...");
+        Student student = new Student("Paul", "McCarthy", "jl@gmail.com");
+
+        System.out.println("Saving the student...");
+        studentDAO.save(student);
+
+        System.out.println("Saved student. Generated Id: " + student.getId());
+    }
+
+    private void getStudentById(StudentDAO studentDAO, int id) {
+        System.out.println("student: ");
+        try {
+            Student student = studentDAO.getById(id);
+            System.out.println(student.toString());
+        } catch (NullPointerException e) {
+            System.out.println("No student found");
+        }
     }
 }
